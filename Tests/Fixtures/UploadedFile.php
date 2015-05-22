52405397d57b5108ce647e6e30ddd35690503cf0
Kévin Dunglas <dunglas@gmail.com>
2015-05-22 23:27:01 +0200

    Add license headers

--- Tests/Fixtures/UploadedFile.php
+++ Tests/Fixtures/UploadedFile.php
@@ -1,8 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Bridge\PsrHttpMessage\Tests\Fixtures;
 
-use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UploadedFileInterface;
 
 /**
